7115ccb1f7b67a23542bac5fdbb569e5e8529b7c
James Andersen <james.andersen@universalmind.com>
2017-05-10 23:41:40 -0600

    Initialize units for each mode

--- gosudoku_test.go
+++ gosudoku_test.go
@@ -2,15 +2,26 @@ package gosudoku
 
 import "testing"
 
-func TestSudokuBoardInit(t *testing.T) {
+func TestStandardSudokuBoardInit(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode.")
 	} else {
 		board := SudokuBoard{}
 		board = board.Init(STANDARD)
-		if unitsLength := len(board.units); unitsLength != 1 {
-			t.Error("Should have single value")
+		if unitsLength := len(board.all_units); unitsLength != 27 {
+			t.Error("Standard Sudoku board should test 27 units")
 		}
 	}
+}
 
+func TestDiagonalSudokuBoardInit(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	} else {
+		board := SudokuBoard{}
+		board = board.Init(DIAGONAL)
+		if unitsLength := len(board.all_units); unitsLength != 29 {
+			t.Error("Diagonal Sudoku board should test 29 units")
+		}
+	}
 }
